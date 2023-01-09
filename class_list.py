1e8ab4b0e1462c661430e81921732da2f18fbc86
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-01-09 22:41:45 +0200

    draft 0.1

---
+++ class_list.py
@@ -0,0 +1,248 @@
+import re
+import sys
+from collections import UserDict
+from datetime import datetime, date, timedelta
+
+
+class Field():
+    def __init__(self) -> None:
+        pass
+
+
+class Name(Field):
+    def __init__(self) -> None:
+        Field.__init__(self)
+        self.__value = None
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        if (new_value != None) and (new_value != ""):
+            self.__value = new_value
+        else:
+            print("Enter value!")
+
+
+class Phone(Field):
+    def __init__(self) -> None:
+        Field.__init__(self)
+        self.__value = None
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value: str):
+        try:
+            result = re.search(
+                r"[+380]?[(]?[0-9]{2}[)]?[0-9]{3}[-]?[0-9]{1,2}[-]?[0-9]{2,3}\b", new_value).string
+            if result != None:
+                self.__value = str(result)
+            else:
+                print("This is not a phone!")
+        except ValueError as e:
+            print(e.error)
+
+
+class Birthday(Field):
+    def __init__(self):
+        self.__value = None
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        if (type(new_value) == datetime) or (type(new_value) == date):
+            self.__value = new_value
+        elif type(new_value) == str:
+            try:
+                if "-" in new_value:
+                    d_list = new_value.split("-")
+                elif "/" in new_value:
+                    d_list = new_value.split("/")
+                elif "." in new_value:
+                    d_list = new_value.split(".")
+                if len(d_list[0]) == 4:
+                    self.__value = datetime(
+                        year=int(d_list[0]), month=int(d_list[1]), day=int(d_list[2]))
+                else:
+                    self.__value = datetime(
+                        year=int(d_list[-1]), month=int(d_list[-2]), day=int(d_list[-3]))
+            except:
+                print("This is not a date! Date not save!")
+
+
+class Record():
+    def __init__(self, name: Name, phone, date: Birthday = None) -> None:
+        self.name = name
+        self.date = date
+        self.phone = phone
+
+    def add_phone(self, phone_new):
+        self.phone.append(Phone(phone_new))
+
+    def change_phone(self, phone_new):
+        self.phone.extend(Phone(phone_new))
+
+    def delete_phone(self, phone_new):
+        try:
+            self.phone.remove(Phone(phone_new))
+        except:
+            print("This phone not found!")
+
+    def days_to_birthday(self):
+        if self.date == None:
+            print("Birthday not entering!")
+            return None
+        else:
+            now_day = datetime.now().date()
+            birthday_now = datetime(
+                year=2023, month=self.date.value.month, day=self.date.value.day).date()
+            days_count = birthday_now - now_day
+            if days_count.days > 365:
+                days_count = days_count - timedelta(days=365)
+            return days_count
+
+
+class AddressBook(UserDict):
+    def __init__(self) -> None:
+        UserDict.__init__(self)
+        # self.on_pages = 2
+        self.number_record = 0
+        self.current_page = 1
+        self.numbers_record = 0
+
+    def iterator(self, on_pages):
+        result = []
+        self.numbers_record = len(self.data)
+        if (len(self.data) % on_pages) == 0:
+            max_page = int(self.numbers_record/on_pages)
+        else:
+            max_page = int(self.numbers_record//on_pages + 1)
+        try:
+            for i, (key_name, val) in enumerate(self.data.items()):
+                i += 1
+                if (i <= self.current_page*on_pages) and (i > (self.current_page-1)*on_pages):
+                    k = key_name.title()
+                    result.append(k)
+                    self.number_record += 1
+                    if self.data[key_name].date.value != None:
+                        d = str(self.data[key_name].date.value.date())
+                        result.append(d)
+                    phone_l = self.data[key_name].phone
+                    for h in phone_l:
+                        result.append(str(h.value))
+                        result.append("\n")
+                if (self.current_page*on_pages <= self.number_record) or (self.number_record == self.numbers_record):
+                    if self.current_page < max_page:
+                        self.current_page += 1
+                    else:
+                        self.current_page = 1
+                    yield (" ".join(result))
+                    result.clear()
+            self.current_page = 1
+            self.number_record = 0
+        except Exception as e:
+            print("Error!", e.args)
+
+    def add_record(self, record: Record):
+        """Функція додання запису"""
+        key = str(record.name.value)
+        if (key == "") or (len(record.phone) == 0):
+            print("Give me name and phone please!")
+            return None
+        try:
+            self.data[key] = record
+            print("Contact save fine!")
+        except:
+            print("Error!")
+
+    def change_record(self, record: Record):
+        """Функція зміни запису"""
+        key = str(record.name.value)
+        if (key == "") or (len(record.phone) == 0):
+            print("Give me name and phone please!")
+            return None
+        try:
+            self.data[key] = record
+            print("Contact save fine!")
+        except:
+            print("There is no user with this name!")
+
+    def search_phone(self, name) -> str:
+        """Функція пошуку телефону за ім'ям"""
+        if (name == ""):
+            print("Give me name please!")
+            return None
+        try:
+            result = []
+            for p in self.data[name].phone:
+                result.append(str(p.value))
+            print(" ".join(result))
+        except:
+            print("There is no user with this name!")
+
+    def show_all(self) -> str:
+        """Функція відображення списку контактів"""
+        try:
+            result = []
+            for key_name in self.data.keys():
+                k = key_name.title()
+                result.append(k)
+                if self.data[key_name].date.value != None:
+                    d = str(self.data[key_name].date.value.date())
+                    result.append(d)
+                phone_l = self.data[key_name].phone
+                for i in phone_l:
+                    result.append(str(i.value))
+                result.append("\n")
+            print(" ".join(result))
+        except Exception as e:
+            print("Error!", e.args)
+
+    def search_record(self, name) -> Record:
+        for key_name in self.data.keys():
+            if key_name == name:
+                return self.data[key_name]
+
+    def packaged_in_dict(self) -> dict:
+        """Функція перетворення адресної книги на словник"""
+        try:
+            result = {}
+            phone_result = []
+            for key_name in self.data.keys():
+                k = key_name.title()
+                result["name"] = k
+                if self.data[key_name].date.value != None:
+                    d = str(self.data[key_name].date.value.date())
+                    result["birthday"] = d
+                phone_l = self.data[key_name].phone
+                for i in phone_l:
+                    phone_result.append(str(i.value))
+                result["phone"] = phone_result
+            return result
+        except Exception as e:
+            print("Error!", e.args)
+
+    def unpackaged_from_dict(self) -> dict:
+        """Функція перетворення словника на адресну книгу"""
+        try:
+
+
+class User():
+    def __init__(self):
+        pass
+
+    def command_hello(self):
+        """Функція привітання"""
+        print("How can I help you?")
+
+    def command_exit(self):
+        """Функція виходу"""
+        sys.exit("Good bye!")
---
+++ main.py
@@ -0,0 +1,119 @@
+import json
+import re
+from collections import UserDict
+from class_list import Field, Name, Phone, Record, AddressBook, User, Birthday
+
+
+address_book = AddressBook()
+user_1 = User()
+command_list = ["hello", "add", "change",
+                "phone", "show all", "close", "exit", "good bye", "birthday", "on page", "search", "save"]
+# with open("data.json", "r") as fh:
+#     address_book = json.load(fh)
+while True:
+    command_name = Name()
+    command_birthday = Birthday()
+# ----------------------------Розпізнавання введенної команди-----------------------
+    command_string = input("Enter command:").lower()
+    if command_string == ".":
+        break
+    find_command = False
+    for k in command_list:
+        if k in command_string:
+            input_com = k
+            attribute_sring = command_string.replace(input_com, "").strip()
+            find_command = True
+            break
+    if not find_command:
+        print("Command undefined! Try again!")
+        continue
+    input_list = attribute_sring.split(" ")
+# ------------------------------Пошук імені -----------------------------------------------
+    if (input_com == "add") or (input_com == "change") or (input_com == "phone") or (input_com == "birthday"):
+        for i in input_list:
+            if i.isalpha():
+                name = i
+                command_name.value = i
+                input_list.remove(i)
+                break
+# ------------------------------Пошук телефону------------------------------------------------
+    if (input_com == "add") or (input_com == "change"):
+        command_phone = []
+        phone_id = 0
+        s = re.findall(
+            r"[+380]?[(]?[0-9]{2}[)]?[0-9]{3}[-]?[0-9]{1,2}[-]?[0-9]{2,3}\b", " ".join(input_list))
+        if s != None:
+            for i in s:
+                i_phone = Phone()
+                i_phone.value = i
+                command_phone.append(i_phone)
+                input_list = (" ".join(input_list).replace(i, "")).split(" ")
+# ------------------------------Пошук дати народження---------------------------------------------
+        date_birthday = None
+        for i in input_list:
+            if (re.search(r"[0-9]{4}[-]?[/]?[.]?[0-9]{2}[-]?[/]?[.]?[0-9]{2}\b", i) != None):
+                date_birthday = re.search(
+                    r"[0-9]{4}[-]?[/]?[.]?[0-9]{2}[-]?[/]?[.]?[0-9]{2}\b", i).string
+                break
+            if (re.search(r"[0-9]{2}[-]?[/]?[.]?[0-9]{2}[-]?[/]?[.]?[0-9]{4}\b", i) != None):
+                date_birthday = re.search(
+                    r"[0-9]{2}[-]?[/]?[.]?[0-9]{2}[-]?[/]?[.]?[0-9]{4}\b", i).string
+                break
+        command_birthday.value = date_birthday
+# ----------------------------Виконання команди--------------------------------------
+    if input_com == "hello":
+        user_1.command_hello()
+    elif (input_com == "close") or (input_com == "exit") or (input_com == "good bye"):
+        user_1.command_exit()
+    elif input_com == "add":
+        try:
+            if (not name) or (len(command_phone) == 0):
+                print("Give me name and phone please!")
+                continue
+            new_record = Record(command_name, command_phone, command_birthday)
+            address_book.add_record(new_record)
+        except:
+            print("Give me name and phone please!")
+    elif input_com == "change":
+        try:
+            if (not name) or (len(command_phone) == 0):
+                print("Give me name and phone please!")
+                continue
+            command_record = address_book.search_record(name)
+            command_name = command_record.name
+            if command_birthday.value == None:
+                command_birthday = command_record.date
+            new_record = Record(command_name, command_phone, command_birthday)
+            address_book.change_record(new_record)
+        except:
+            print("Give me name and phone please!")
+    elif input_com == "phone":
+        try:
+            address_book.search_phone(name)
+        except:
+            print("Enter user name!")
+    elif input_com == "show all":
+        try:
+            address_book.show_all()
+        except:
+            print("No data!")
+    elif input_com == "birthday":
+        try:
+            command_record = address_book.search_record(name)
+            print("by", command_record.days_to_birthday().days, "days")
+        except:
+            print("No date!")
+    elif input_com == "on page":
+        try:
+            records_on_pages = int(input("Input count records on pages:"))
+            n_p_b = 1
+            for p_b in address_book.iterator(records_on_pages):
+                print("Pages:", n_p_b, p_b)
+                n_p_b += 1
+        except:
+            print("No data!")
+    else:
+        print("Command undefined! Try again!")
+# print(address_book.save_dict())
+with open("data.json", "a") as fh:
+    json.dump(address_book.packaged_in_dict(), fh)
